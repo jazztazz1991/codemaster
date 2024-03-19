27e96329b353b143dfd8d1f7e3a29855282e3a5c
jsong73 <jessicajinsong@gmail.com>
2024-03-18 21:49:32 -0400

    styling for exercises page

--- client/src/pages/exercises/index.js
+++ client/src/pages/exercises/index.js
@@ -7,13 +7,23 @@ import {
     CardTitle,
     CardContent
 } from "@/components/ui/card"
+import { GiPlainCircle , GiPlainSquare } from "react-icons/gi";
+import { IoTriangleSharp } from "react-icons/io5";
+import { useState } from "react";
 
 
-export default function Exercises({ easyExercises, mediumExercises, hardExercises }) {
+export default function Exercises({ easyExercises, mediumExercises, hardExercises, exercises }) {
+
+
     return (
-        <Layout>
-            <div className="hero" id="exercisesPage">
-                <h2 className="mt-6 text-lg leading-8 text-gray-900">Easy Exercises</h2>
+        <Layout className="">
+            <div className="" id="exercisesPage">
+            <CardHeader className="mb-8">
+                    <CardTitle className="text-center text-4xl mt-10 mb-4 font-bold"> ✨ Explore the Javascript exercises on Code Master ✨</CardTitle>
+
+                    <h1 className="text-center text-xl">Work through each exercise to improve your skills. They’re great practice and fun to do! </h1>
+            </CardHeader>
+                
                 <div className="flex grid grid-cols-2 items-center">
                     {easyExercises.map(exercise => (
                         <div key={exercise._id}>
@@ -21,7 +31,7 @@ export default function Exercises({ easyExercises, mediumExercises, hardExercise
                         </div>
                     ))}
                 </div>
-                <h2 className="mt-6 text-lg leading-8 text-gray-900">Medium Exercises</h2>
+
                 <div className="flex grid grid-cols-2 items-center">
                     {mediumExercises.map(exercise => (
                         <div key={exercise._id}>
@@ -29,7 +39,7 @@ export default function Exercises({ easyExercises, mediumExercises, hardExercise
                         </div>
                     ))}
                 </div>
-                <h2 className="mt-6 text-lg leading-8 text-gray-900">Hard Exercises</h2>
+
                 <div className=" flex grid grid-cols-2 items-center">
                     {hardExercises.map(exercise => (
                         <div key={exercise._id}>
@@ -37,6 +47,8 @@ export default function Exercises({ easyExercises, mediumExercises, hardExercise
                         </div>
                     ))}
                 </div>
+        
+            <div className="exercises-margin" style={{ marginBottom: '100px' }}></div>
             </div>
         </Layout>
     )
@@ -50,32 +62,57 @@ export async function getStaticProps() {
     const mediumExercises = exercises.filter(exercise => exercise.difficulty.toLowerCase()  === "medium");
     const hardExercises = exercises.filter(exercise => exercise.difficulty.toLowerCase()  === "hard");
 
-    return { props: { easyExercises, mediumExercises, hardExercises } }
+    return { props: { easyExercises, mediumExercises, hardExercises, exercises } }
 }
 
 export function ExerciseCard({ exercise }) {
+    // console.log(exercise)
  
+    const [ previewQuestion ] = exercise.question.split(".");
+    // console.log(previewQuestion)
     return (
-        <Card key={exercise._id} className="m-5">
+        <Link href={`/exercises/${exercise._id}`} > 
+          
+        <Card key={exercise._id} className="m-5 shadow-xl exercise-card" style={{ height: '200px', borderRadius: '14px' }}>
             <div className="">
                 <CardHeader>
-                    <CardTitle>{exercise.title}</CardTitle>
+                        <CardTitle>{exercise.title}</CardTitle>
                 </CardHeader>
-                <CardContent className="flex grid grid-cols-6 items-center space-x-4 sm:space-x-10">
-                    <div>
-                        {exercise.difficulty === "easy" ? <p className="flex col-spa-1 items-center text-balance">Difficulty: <span id="easy">{exercise.difficulty}</span></p> : null}
-                        {exercise.difficulty === "medium" ? <p className="flex col-spa-1 items-center text-balance">Difficulty: <span id="medium">{exercise.difficulty}</span></p> : null}
-                        {exercise.difficulty === "hard" ? <p className="flex col-spa-1 items-center text-balance">Difficulty: <span id="hard">{exercise.difficulty}</span></p> : null}
-                    </div>
+
+                <CardContent className="flex grid-cols-6 px-14 items-center space-x-4 sm:space-x-10">
+                        {exercise.difficulty === "easy" ? 
+                            <p className="flex col-spa-1 items-center text-balance">
+                                <span className="bg-green-100 border border-green-500 px-3 py-2 rounded-full flex items-center font-bold text-green-600 text-sm"id="easy">
+                                    <GiPlainCircle className="text-green-600 mr-1.5 h-2.5 w-2.5" />  
+                                    {exercise.difficulty} 
+                                </span>
+                            </p> : null}
+
+                        {exercise.difficulty === "medium" ? 
+                            <p className="flex col-spa-1 items-center text-balance">
+                                <span className="bg-yellow-100 border border-yellow-500 px-3 py-2 rounded-full flex items-center font-bold text-yellow-600 text-sm"id="easy">
+                                    <GiPlainSquare className="text-yellow-600 mr-1.5 h-2.5 w-2.5" />  
+                                    {exercise.difficulty} 
+                                </span>
+                            </p> : null}
+
+
+                        {exercise.difficulty === "hard" ? 
+                            <p className="flex col-spa-1 items-center text-balance">
+                                <span className="bg-red-100 border border-red-500 px-3 py-2 rounded-full flex items-center font-bold text-red-600 text-sm"id="easy">
+                                    <IoTriangleSharp className="text-red-600 mr-1.5 h-3 w-3" />  
+                                    {exercise.difficulty} 
+                                </span>
+                            </p> : null}
+
+
                     <div className="col-span-4">
-                        <p className="inset-y-0 right-0 flex items-center col-span-4 pr-2 sm:ml-6 sm:pr-0">{exercise.question}</p>
+                        <p className="flex items-center text-left text-zinc-600 pr-2 sm:ml-6 sm:pr-0">{previewQuestion}</p>
                     </div>
-                    <div>
-                        <Link href={`/exercises/${exercise._id}`} className="text-gray-300 col-span-1 bg-gray-700 hover:text-white rounded-lg px-3 py-2 text-sm font-medium flex items-center justify-center">Begin Exercise</Link>
-                    </div>
+
                 </CardContent>
             </div>
-
         </Card>
+    </Link>
     )
 }
